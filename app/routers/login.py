39e4ff1b8ca92f503adfcb09cabbaf304e9287d5
IvanBozoCa <133916421+IvanBozoCa@users.noreply.github.com>
2025-06-04 16:11:45 -0400

    Nuevas Actualizaciones

--- app/routers/login.py
+++ app/routers/login.py
@@ -19,7 +19,7 @@ def login(data: schemas.LoginRequest, db: Session = Depends(get_db)):
     if not usuario:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Credenciales inválidas")
 
-    if not auth.verificar_contrasena(data.password, usuario.contrasena):
+    if not auth.verificar_contrasena(data.contrasena, usuario.contrasena):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Credenciales inválidas")
 
     token = auth.crear_token({
