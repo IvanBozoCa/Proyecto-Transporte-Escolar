260473f5bbe29235bc9150b6ab83aa5b009eb434
IvanBozoCa <133916421+IvanBozoCa@users.noreply.github.com>
2025-06-25 14:52:53 -0400

    finales de firebase en render

--- app/firebase/firebase_config.py
+++ app/firebase/firebase_config.py
@@ -1,20 +1,24 @@
 import os
+from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials, db
+
+# Cargar variables del .env
+load_dotenv()
 
 def initialize_firebase():
     if not firebase_admin._apps:
         entorno = os.getenv("ENV", "local")
 
-        if entorno == "render":
-            cred_path = "/etc/secrets/firebase_key_render.json"
-        else:
-            cred_path = os.getenv("FIREBASE_KEY_PATH")
+        # Trabajamos SOLO en entorno local
+        if entorno != "local":
+            raise ValueError("Este archivo solo debe usarse en entorno local")
 
+        cred_path = os.getenv("FIREBASE_KEY_PATH")
         db_url = os.getenv("FIREBASE_DATABASE_URL")
 
         if not cred_path or not os.path.isfile(cred_path):
-            raise ValueError("No se encontró el archivo de clave de Firebase")
+            raise ValueError(f"No se encontró el archivo de clave de Firebase en la ruta: {cred_path}")
 
         if not db_url:
             raise ValueError("Falta la URL de la base de datos de Firebase")
@@ -23,3 +27,5 @@ def initialize_firebase():
         firebase_admin.initialize_app(cred, {
             'databaseURL': db_url
         })
+
+        print("Firebase inicializado correctamente en entorno local.")
