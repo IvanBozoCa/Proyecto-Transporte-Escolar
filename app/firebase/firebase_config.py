cfb667d316867c7057c3e11b6a079ac0d3a09b43
IvanBozoCa <133916421+IvanBozoCa@users.noreply.github.com>
2025-06-25 00:01:54 -0400

    implementacion de firebase, para finalizar ruta

--- app/firebase/firebase_config.py
+++ app/firebase/firebase_config.py
@@ -1,12 +1,12 @@
-import firebase_admin
-from firebase_admin import credentials, messaging
 import os
+import firebase_admin
+from firebase_admin import credentials
 
-# Ruta relativa al archivo JSON
-ruta_credenciales = "C:/Users/Ivan/Desktop/universidad/año 2025/curso proyecto de tesis/Proyecto/transporte_backend/app/firebase/firebase_key.json"
 def initialize_firebase():
     if not firebase_admin._apps:
+        ruta_credenciales = os.path.join(os.path.dirname(__file__), 'firebase_key.json')
+
         cred = credentials.Certificate(ruta_credenciales)
         firebase_admin.initialize_app(cred, {
-            "databaseURL": "https://transporte-escolar-543e8-default-rtdb.firebaseio.com/"
+            'databaseURL': os.getenv("FIREBASE_DATABASE_URL", "https://transporte-escolar-543e8-default-rtdb.firebaseio.com/")  # <-- reemplaza esto
         })
